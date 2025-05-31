3b7b8240694c142720159aafd4e8912005df1295
LeonFray <143506006+Leon-Fray@users.noreply.github.com>
2025-05-31 14:46:12 -0500

    Add files via upload

---
+++ config.template.js
@@ -0,0 +1,6 @@
+// This file will be replaced during build with actual API key
+window.CONFIG = {
+    ELEVENLABS_API_KEY: 'PLACEHOLDER_API_KEY',
+    ELEVENLABS_VOICE_ID: 'rbFGGoDXFHtVghjHuS3E' // French lady
+};
+
